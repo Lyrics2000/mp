847392017582595cb71999f3859f42da3632efab
Achongo A. Thomas <tachongo@BRITAMGROUP.COM>
2025-03-13 18:00:36 +0300

    Amm1

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -13,7 +13,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'payments.Middleware.AzureADAuthenticationMiddleware',
+
 ]
 
 DATABASES = {
