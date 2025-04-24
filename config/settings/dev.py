1b6e906a32350a1979384f86c3ea39ed74bca70d
Achongo A. Thomas <tachongo@BRITAMGROUP.COM>
2025-04-24 12:56:37 +0300

    Amm12

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -57,6 +57,7 @@ C2B_CONFIRMATION_URL = "https://brtgw.britam.com/mp/payment/uat/api/v1/confirmat
 C2B_VALIDATE_URL = "https://brtgw.britam.com/mp/payment/uat/api/v1/validation_url/"
 
 
+
 AUDIENCE = "api://2551a6ae-039d-43bb-b05e-8d97d07f15e2"
 ISSUER_ID = "e303f219-75ef-479a-b23c-35ac9479a8ce"
 
