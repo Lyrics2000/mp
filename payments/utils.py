bc474dd3497ad26b8497c658404c3f256280b8a2
Achongo A. Thomas <tachongo@BRITAMGROUP.COM>
2025-04-24 14:56:30 +0300

    Amm1

--- payments/utils.py
+++ payments/utils.py
@@ -19,43 +19,54 @@ class C2BSendMessages:
     def sendCallbacks(self, confirmation_obj):
         check_url = Callbackhanldlers.objects.filter(
             paybill=confirmation_obj.BusinessShortCode
-        ).first()
+        )
 
-        if check_url:
-            if re.match(rf"{check_url.regex}", confirmation_obj.BillRefNumber):
-                data = {
-                    "Body": {
-                        "stkCallback": {
-                            "MerchantRequestID": "84115-769684-1",
-                            "CheckoutRequestID": confirmation_obj.BillRefNumber,
-                            "ResultCode": 0,
-                            "ResultDesc": "The service request is processed successfully.",
-                            "CallbackMetadata": {
-                                "Item": [
-                                    {"Name": "Amount", "Value": float(confirmation_obj.TransAmount)},
-                                    {"Name": "MpesaReceiptNumber", "Value": confirmation_obj.TransID},
-                                    {"Name": "Balance"},
-                                    {"Name": "TransactionDate", "Value": confirmation_obj.TransTime},
-                                    {"Name": "PhoneNumber", "Value": confirmation_obj.MSISDN}
-                                ]
+        if len(check_url) > 0:
+            for i in check_url:
+                if re.match(rf"{i.regex}", i.BillRefNumber):
+                    data = {
+                        "Body": {
+                            "stkCallback": {
+                                "MerchantRequestID": "84115-769684-1",
+                                "CheckoutRequestID": i.BillRefNumber,
+                                "ResultCode": 0,
+                                "ResultDesc": "The service request is processed successfully.",
+                                "CallbackMetadata": {
+                                    "Item": [
+                                        {"Name": "Amount", "Value": float(i.TransAmount)},
+                                        {"Name": "MpesaReceiptNumber", "Value": i.TransID},
+                                        {"Name": "Balance"},
+                                        {"Name": "TransactionDate", "Value": i.TransTime},
+                                        {"Name": "PhoneNumber", "Value": i.MSISDN}
+                                    ]
+                                }
                             }
                         }
                     }
-                }
-                try:
-                    ap = HttpCalls()
-                    response = ap.post(data, check_url.url)
-                    C2BCallbackResponses.objects.create(
-                        callbackhandler=check_url,
-                        sent=True,
-                        response=response.text
-                    )
-                    confirmation_obj.confirmation_status = True
-                    confirmation_obj.save()
+                    try:
+                        ap = HttpCalls()
+                        response = ap.post(data, check_url.url)
+                        
+                        try:
+                            if response.status_code ==  200:
+                                json = response.json()
+                                if json['ResponseCode'] == 0:
 
-                except Exception as e:
-                    print(f"There is an error sending data: {e}")
-            else:
-                print("BillRefNumber does not match the required pattern.")
+                                    C2BCallbackResponses.objects.create(
+                                        callbackhandler=check_url,
+                                        sent=True,
+                                        response=response.text
+                                    )
+                                    confirmation_obj.confirmation_status = True
+                                    confirmation_obj.save()
+                        except Exception as e:
+                            print(f"There is an error saving data: {e}")
+
+
+                    except Exception as e:
+                        print(f"There is an error sending data: {e}")
+                else:
+                    print("BillRefNumber does not match the required pattern.")
+        
         else:
             print("No matching callback handler found for the provided paybill.")
